bb1835379e9f9ec70a71168a04b8def5b6f2f216
Kwazi.Mashaya <Kwazi.Mashaya@INSPD3RZ5HV2>
2025-06-20 11:56:49 +0200

    Add project files.

---
+++ AuthSystem-master/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AuthSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthSystem.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { } 
+        
+    }
+}
